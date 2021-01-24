63d426b343b5303876cc99d975e4649f0bc84a9b
Smith Aitufe <smithaitufe@live.com>
2021-01-24 21:26:44 +0100

    added variable declarations

---
+++ src/variable.js
@@ -0,0 +1,26 @@
+// a variable is named memory location. it stores value that can change during runtime. the value can be reassigned.
+// a variable should begin with a letter or underscore.
+// it can contain a combination of letters, numbers and underscore.
+// the name should be meaningful. that is it should relate to the value that it will contain.
+
+// name 
+// firstName
+// lastName
+// sex
+// gender
+// age
+
+
+var name = "Damilola" //declare and initialize
+var firstName = "Damilola"
+let lastName = "Okoro"
+
+let gender = "Female"
+
+let addressLine1 = "Magodo Estate"
+let addressLine2 = "Lagos State"
+
+
+console.log(name, firstName, lastName)
+name = "Adams"
+console.log(name, firstName, lastName)
